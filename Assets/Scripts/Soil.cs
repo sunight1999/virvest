4cc44a7cb8326b68dbae86b5809a16e10cf439c1
phiprip-Choi <132041240+phiprip-Choi@users.noreply.github.com>
2023-10-18 16:16:36 +0900

    수정

--- Assets/Scripts/Soil.cs
+++ Assets/Scripts/Soil.cs
@@ -4,28 +4,16 @@ using UnityEngine;
 
 public class Soil : MonoBehaviour
 {
-    private bool onShovel = false;
-    GameObject onShovelPos = null;
-    private void Update()
-    {
-        if (onShovel)
-        {
-            transform.position = onShovelPos.transform.position;
-            transform.rotation = onShovelPos.transform.rotation;
-        }
-    }
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.name == "Shovel_Boxcollider")
         {
             collider.gameObject.GetComponent<BoxCollider>().enabled = false;
-            onShovelPos = collider.gameObject.transform.GetChild(0).gameObject;
-            onShovel =true;
-            /*
-            var colobj = collider.gameObject.transform.GetChild(0).gameObject.transform;
-            Instantiate(gameObject, colobj.position, colobj.rotation, collider.gameObject.transform.parent);
-            Destroy(gameObject);*/
-            //transform.position = collider.gameObject.transform.GetChild(0).position;
+            
+            var shovelPos = collider.gameObject.transform.GetChild(0).transform;
+            transform.position = shovelPos.position;
+            transform.rotation = shovelPos.rotation;
+            transform.SetParent(shovelPos);
         }
     }
 }
