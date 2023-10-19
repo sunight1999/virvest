6f86c81cad4fe9a735a2cf9d4b3beeb43e099219
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2023-10-20 08:00:16 +0900

    문 UI 구현

--- Assets/Scripts/ActiveTeleportationRay.cs
+++ Assets/Scripts/ActiveTeleportationRay.cs
@@ -14,11 +14,18 @@ public class ActiveTeleportationRay : MonoBehaviour
     public InputActionProperty leftActive;
     public InputActionProperty rightActive;
 
+    public XRRayInteractor leftRay;
+    public XRRayInteractor rightRay;
+
     void Update()
     {
         leftController.SetActive(leftActive.action.ReadValue<float>() > 0.1f);
         rightController.SetActive(rightActive.action.ReadValue<float>() > 0.1f);
-        leftDirectController.SetActive(leftActive.action.ReadValue<float>() == 0);
-        rightDirectController.SetActive(rightActive.action.ReadValue<float>() == 0);
+
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+        leftDirectController.SetActive(!isLeftRayHovering && leftActive.action.ReadValue<float>() == 0);
+
+        bool isRightRayHovering = leftRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
+        rightDirectController.SetActive(!isRightRayHovering && rightActive.action.ReadValue<float>() == 0);
     }
 }
--- Assets/Scripts/SceneLoadManager.cs
+++ Assets/Scripts/SceneLoadManager.cs
@@ -1,3 +1,4 @@
+using Meta.WitAi;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,32 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoadManager : MonoBehaviour
 {
+    public GameObject housePanel;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name == "XR Origin")
         {
-            if(SceneManager.GetActiveScene().buildIndex == 0) SceneManager.LoadScene(1);
-            else if(SceneManager.GetActiveScene().buildIndex == 1) SceneManager.LoadScene(0);
+            housePanel.SetActive(true);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "XR Origin")
+        {
+            housePanel.SetActive(false);
+        }
+    }
+
+    public void OffPanel()
+    {
+        housePanel.SetActive(false);
+    }
+
+    public void SceneChange()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0) SceneManager.LoadScene(1);
+        else if (SceneManager.GetActiveScene().buildIndex == 1) SceneManager.LoadScene(0);
     }
 }
