6944eefa3359128c47f0b1dfa21549b2606148b0
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2023-10-20 09:58:43 +0900

    시연 영상 촬영 전 마지막 점검

--- Assets/Scripts/ActiveTeleportationRay.cs
+++ Assets/Scripts/ActiveTeleportationRay.cs
@@ -19,13 +19,13 @@ public class ActiveTeleportationRay : MonoBehaviour
 
     void Update()
     {
-        leftController.SetActive(leftActive.action.ReadValue<float>() > 0.1f);
-        rightController.SetActive(rightActive.action.ReadValue<float>() > 0.1f);
-
         bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
         leftDirectController.SetActive(!isLeftRayHovering && leftActive.action.ReadValue<float>() == 0);
 
         bool isRightRayHovering = leftRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
         rightDirectController.SetActive(!isRightRayHovering && rightActive.action.ReadValue<float>() == 0);
+
+        leftController.SetActive(!isLeftRayHovering && leftActive.action.ReadValue<float>() > 0.1f);
+        rightController.SetActive(!isRightRayHovering && rightActive.action.ReadValue<float>() > 0.1f);
     }
 }
