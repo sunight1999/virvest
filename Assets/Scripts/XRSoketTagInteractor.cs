6006a566faa2161ef8239ed5b4b91a1b41d16a7e
phiprip-Choi <132041240+phiprip-Choi@users.noreply.github.com>
2023-10-31 20:24:01 +0900

    소켓 오브젝트 한정 코드

---
+++ Assets/Scripts/XRSoketTagInteractor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRSoketTagInteractor : XRSocketInteractor
+{
+    //특정 오브젝트 미리보기 소켓
+    public string targetTag;
+
+    public override bool CanHover(IXRHoverInteractable interactable)
+    {
+        return base.CanHover(interactable) && interactable.transform.tag == targetTag;
+    }
+
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        return base.CanSelect(interactable) && interactable.transform.tag == targetTag;
+    }
+}
