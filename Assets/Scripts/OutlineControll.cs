29bd3d3a12f42f8fbbcbc6a56754eff1d71759cf
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-07-03 19:11:31 +0900

    개발 제출 전 수정

--- Assets/Scripts/OutlineControll.cs
+++ Assets/Scripts/OutlineControll.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class OutlineControll : MonoBehaviour
 {
     Material material;
+    FarmManager fm;
     
     void Start()
     {
         material = GetComponent<MeshRenderer>().material;
         material.SetFloat("_OutlineWidth", 0);
+        fm = FindAnyObjectByType<FarmManager>();
     }
 
     public void OnOutline()
@@ -20,5 +22,11 @@ public class OutlineControll : MonoBehaviour
     public void OffOutline()
     {
         material.SetFloat("_OutlineWidth", 0);
+    }
+
+    private void Update()
+    {
+        if (fm.isGrab)
+            OffOutline();
     }
 }
